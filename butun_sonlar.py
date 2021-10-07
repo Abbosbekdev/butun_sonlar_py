9c6fe6a1925c2667f32fb559f5f71a642ea6de38
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-07 09:22:33 +0500

    Create butun_sonlar.py
    python dasturlash tilida butun sonlar

---
+++ butun_sonlar.py
@@ -0,0 +1,123 @@
+#1-masala
+#x=float(input('x = '))
+#print(x/100)
+
+#2-masala
+#x=float(input('x = '))
+#print(x/1000)
+
+#3-masala
+#x=int(input('x = '))
+#print(x//1024)
+
+#4-masala
+#a,b=int(input('a = ')),int(input('b = '))
+#print(a//b)
+
+#5-masala
+
+#a,b=int(input('a = ')),int(input('b = '))
+#print(a//b,'ta joylashtirsa boladi')
+#print(a%b,'ta joylashtirib bolmaydi')
+
+#6-masala
+
+#a=int(input('x = '))
+#print('birliklar xonasidagi son = ',a%10)
+#print('onliklar xonasidagi son =  ',a//10)
+
+#7-masala
+
+#x=int(input('x = '))
+#print(x//10+x%10)
+
+#8-masala
+
+#x=int(input('x = '))
+#print(x%10*10+x//10)
+#print(x-9*(x//10-x%10))
+
+#9-masala
+
+#x=int(input('x = '))
+#print(x//100)
+
+#10-masala
+
+#x=int(input('x = '))
+#print('birlar xonasi : ',x%10)
+#print("o'nlar xonasi : ",x%100//10)
+
+#11-masala
+
+#x=int(input('x = '))
+#print(x//100+x%100//10+x%10)
+
+#12-masala
+
+#x = int(input('x = '))
+#print(x%10*100+x%100//10*10+x//100)
+
+#13-masala
+
+#x=int(input('x ='))
+#print(x//100+x%100*10)
+
+#14-masala
+
+#x=int(input('x = '))
+#print(x//10+x%10*100)
+
+#15-masala
+
+#x=int(input('x = '))
+#print(x//100*10+x%100//10*100+x%10)
+
+#16-masala
+
+#x=int(input('x = '))
+#print(x//100*100+x%10*10+x%100//10)
+
+#17-masala
+import math
+
+#x=int(input('x = '))
+#print(x%1000//100)
+
+#18-masala
+
+#x=int(input('x = '))
+#print(x//10)
+
+#19-masala
+
+#x=int(input('x = '))
+#print(x*60)
+
+#20-masala
+
+#x= int(input('x = '))
+#print(x*3600)
+
+#21-masala
+
+#x=int(input('x = '))
+#print('sekund : ',x)
+#print('minut : ',x*60)
+
+#22-masala
+
+#x=int(input('x = '))
+#print("sekund : ",x)
+#print("soat : ",x*3600)
+
+#24-masala
+
+k=int(input('k = '))
+a = (k - 3) % 7
+d = {1:"dushanba", 2:"seshanba", 3:"chorshanba", 4:"payshanba", 5:'juma', 6:"shnba", 0:"yakshanba"}
+print(d[a])
+
+
+
+
